e9535db9763364f6c0c66c02a33828ed43c7381a
Suraj singh <surajgpes@gmail.com>
2025-07-16 10:11:57 +0000

    images updated to cloudinary

--- next.config.js
+++ next.config.js
@@ -17,6 +17,12 @@ const nextConfig = {
         hostname: 'images.unsplash.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
